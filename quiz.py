cb0542b1a12c7f73201c20ec5cf93b4fbcf18e42
Walter <walter.clark24@yahoo.com>
2023-08-24 16:59:38 -0400

    coded questions

--- quiz.py
+++ quiz.py
@@ -9,16 +9,22 @@ class Quiz:
     answers = []
     
     def __init__(self):
+        question_types = (Add, Multiply)
         # generate 10 random questions with numbers from 1 to 10
+        for _ in range(10):
+            num1 = random.randint(1, 10)
+            num2 = random.randint(1,10)
+            question = random.choice(question_types)(num1, num2)
         # add these questions into self.questions
+            self.questions.append(question)
         
-     def take_quiz(self):
+    def take_quiz(self):
         # log the start time
         # ask all of the questions
         # log if they got the question right
         # log the end time
         # show a summary
-        
+        pass
         def ask(self, question):
         # log the start time
         # capture the answer
@@ -27,12 +33,23 @@ class Quiz:
         # if the answer's right, send back True
         # otherwise, send back False
         # send back the elapsed time, too
+            pass
+            def total_correct(self):
+                total = 0
+                for answer in self.answers:
+                   if answer[0]:
+                        total += 1
+                return total
+           # return the total # of correct answers
         
-         def summary(self):
-         # print how many you got right and the total # of questions. 5/10
-            print("You got {} out of {} right.".format(
+                def summary(self):
+                 # print how many you got right and the total # of questions. 5/10
+                 print("You got {} out of {} right.".format(
                 self.total_correct(), len(self.questions)
-            ))
+                ))
 
               
         # print the total time for the quiz: 30 seconds!
+            print("It took you () seconds total." .format(
+                    (self.end_time-self.start_time).seconds
+            ))
